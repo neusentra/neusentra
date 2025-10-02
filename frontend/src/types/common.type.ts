82c63838662f9025bca85f9ed2a4085684593826
Dhanush S <dhanush6102002@gmail.com>
2025-10-02 23:42:41 +0530

    [feat]: add types for login data

--- frontend/src/types/common.type.ts
+++ frontend/src/types/common.type.ts
@@ -1,11 +1,22 @@
-import type { IUser } from "./user.type";
 
 export interface IUserData {
-    user: object | null;
+    user: {
+        loginId: string;
+        userId: string;
+        role: string;
+        permissions: {
+            can_manage_devices: boolean;
+            can_manage_policies: boolean;
+            can_view_logs: boolean;
+            can_manage_users: boolean;
+            can_manage_network: boolean;
+            can_manage_blocklists: boolean;
+            can_manage_scheduled_tasks: boolean;
+        };
+    };
 }
 
-export interface ILoginResponse {
-    user: IUser;
+export interface ILoginResponse extends IUserData {
     iat: number;
     exp: number;
 }
