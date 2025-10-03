dd7550d8d37de47c74e4e62b0edb4fba302744d0
Muhamed Fazeel K.A <reachme@fazeel.in>
2025-10-03 22:16:38 +0530

    [feat] (redis-cache): implement redis cache module (#7)
    * [chore] (configuration): add new env variables for redis connection
    
    * [chore] (validation): add new validations
    
    * [chore] (constants): add new constants for redis
    
    * [feat] (redis-cache): implement redis cache module
    
    * [chore] (env.example): add new env variables of redis
    
    * [chore] (package): add new packages for redis

--- backend/src/common/constants/constants.ts
+++ backend/src/common/constants/constants.ts
@@ -23,3 +23,7 @@ export const CONTENT_TYPE = {
 export const MAX_INT_LIMIT = 2147483647;
 
 export const PUBLIC_METADATA = 'isPublic';
+
+export const REDIS_DEFAULT_PORT = 6379;
+
+export const REDIS_DEFAULT_TTL = 3600000;
