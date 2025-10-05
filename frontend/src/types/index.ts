66dad368015bf9a38ec69e8595dadbc0d35166eb
Dhanush S <dhanush6102002@gmail.com>
2025-10-05 17:07:24 +0530

    [refactor] (types): add directory exports for types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,4 @@
+export * from './common.type'
+export * from './user.type'
+export * from './providers'
+export * from './api'
---
+++ frontend/src/types/providers/auth.type.ts
@@ -0,0 +1,18 @@
+import type { IUserData, IUserPermissions } from "../common.type";
+
+export interface IAuthContext {
+    userData: IUserData | null;
+    setUserData: (data: IUserData | null) => void;
+    isInitialized: boolean | null;
+    isAuthenticated: boolean;
+    setIsAuthenticated: (authenticated: boolean) => void;
+    setUserDataFromToken: (
+        token: string,
+        permissions: IUserPermissions,
+        skipLocalStorage?: boolean
+    ) => { success: boolean; error: string | null };
+}
+
+export type AuthProviderProps = {
+    children: React.ReactNode;
+};
