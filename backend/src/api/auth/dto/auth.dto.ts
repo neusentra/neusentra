31dfb3d974f4e50a87ffa68c0f2fb9a471751d6d
muhamedfazeel <reachme@fazeel.in>
2025-10-09 21:31:20 +0530

    [chore] [auth dto]: code optimization and lint changes

--- backend/src/api/auth/dto/auth.dto.ts
+++ backend/src/api/auth/dto/auth.dto.ts
@@ -5,61 +5,67 @@ import { SuccessResponseDto } from 'src/common/dto/success-response.dto';
 import { UserPermissionsDto } from 'src/common/dto/user-permission.dto';
 
 export class LoginRequestDto {
-    @ApiProperty({ example: 'groot', description: 'Username of the user' })
-    @IsString()
-    @MinLength(4)
-    @MaxLength(20)
-    username: string;
+  @ApiProperty({ example: 'groot', description: 'Username of the user' })
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
 
-    @ApiProperty({ description: 'Password of the user' })
-    @IsString()
-    @MinLength(8)
-    @MaxLength(16)
-    password: string;
+  @ApiProperty({ description: 'Password of the user' })
+  @IsString()
+  @MinLength(8)
+  @MaxLength(16)
+  password: string;
 }
 
 export class UserInfoDto {
-    @ApiProperty()
-    id: string;
+  @ApiProperty()
+  id: string;
 
-    @ApiProperty()
-    loginId?: string;
+  @ApiProperty()
+  loginId?: string;
 
-    @ApiProperty()
-    fullname: string;
+  @ApiProperty()
+  fullname: string;
 
-    @ApiProperty()
-    username: string;
+  @ApiProperty()
+  username: string;
 
-    @ApiProperty()
-    @Expose({ name: 'password_hash' })
-    passwordHash: string;
+  @ApiProperty()
+  @Expose({ name: 'password_hash' })
+  passwordHash: string;
 
-    @ApiProperty()
-    role: string;
+  @ApiProperty()
+  role: string;
 
-    @ApiProperty({ type: () => UserPermissionsDto })
-    permissions: UserPermissionsDto;
+  @ApiProperty({ type: () => UserPermissionsDto })
+  permissions: UserPermissionsDto;
 }
 
 export class LoginDataDto {
-    @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsIn...' })
-    accessToken: string;
+  @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsIn...' })
+  accessToken: string;
 
-    @ApiProperty({ type: () => UserPermissionsDto })
-    permissions: UserPermissionsDto;
+  @ApiProperty({ type: () => UserPermissionsDto })
+  permissions: UserPermissionsDto;
 }
 
 export class LoginResponseDto extends SuccessResponseDto {
-    @ApiProperty({ type: () => LoginDataDto })
-    declare data: LoginDataDto;
+  @ApiProperty({ type: () => LoginDataDto })
+  declare data: LoginDataDto;
 }
 
+export class PayloadDto {
+  loginId: string;
+  userId: string;
+  name: string;
+  role: string;
+}
 class AccessTokenDto {
-    accessToken: string;
+  accessToken: string;
 }
 
 export class RefreshTokenResponseDto extends SuccessResponseDto {
-    @ApiProperty({ type: () => AccessTokenDto })
-    declare data: AccessTokenDto;
+  @ApiProperty({ type: () => AccessTokenDto })
+  declare data: AccessTokenDto;
 }
