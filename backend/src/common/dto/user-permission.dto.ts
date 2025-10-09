039ddfae2034fb5fc4b781d7668b5409cfda45e6
muhamedfazeel <reachme@fazeel.in>
2025-10-09 21:35:19 +0530

    [lint] [common dto]: lint changes

--- backend/src/common/dto/user-permission.dto.ts
+++ backend/src/common/dto/user-permission.dto.ts
@@ -1,32 +1,28 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Expose } from "class-transformer";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserPermissionsDto {
-    @Expose({ name: 'can_manage_devices' })
-    @ApiProperty({ description: 'Indicates if the user can manage devices.' })
-    canManageDevices: boolean;
+  @ApiProperty({ description: 'Indicates if the user can manage devices.' })
+  canManageDevices: boolean;
 
-    @Expose({ name: 'can_manage_policies' })
-    @ApiProperty({ description: 'Indicates if the user can manage policies.' })
-    canManagePolicies: boolean;
+  @ApiProperty({ description: 'Indicates if the user can manage policies.' })
+  canManagePolicies: boolean;
 
-    @Expose({ name: 'can_view_logs' })
-    @ApiProperty({ description: 'Indicates if the user can view logs.' })
-    canViewLogs: boolean;
+  @ApiProperty({ description: 'Indicates if the user can view logs.' })
+  canViewLogs: boolean;
 
-    @Expose({ name: 'can_manage_users' })
-    @ApiProperty({ description: 'Indicates if the user can manage users.' })
-    canManageUsers: boolean;
+  @ApiProperty({ description: 'Indicates if the user can manage users.' })
+  canManageUsers: boolean;
 
-    @Expose({ name: 'can_manage_network' })
-    @ApiProperty({ description: 'Indicates if the user can manage network settings.' })
-    canManageNetwork: boolean;
+  @ApiProperty({
+    description: 'Indicates if the user can manage network settings.',
+  })
+  canManageNetwork: boolean;
 
-    @Expose({ name: 'can_manage_blocklists' })
-    @ApiProperty({ description: 'Indicates if the user can manage blocklists.' })
-    canManageBlocklists: boolean;
+  @ApiProperty({ description: 'Indicates if the user can manage blocklists.' })
+  canManageBlocklists: boolean;
 
-    @Expose({ name: 'can_manage_scheduled_tasks' })
-    @ApiProperty({ description: 'Indicates if the user can manage scheduled tasks.' })
-    canManageScheduledTasks: boolean;
+  @ApiProperty({
+    description: 'Indicates if the user can manage scheduled tasks.',
+  })
+  canManageScheduledTasks: boolean;
 }
